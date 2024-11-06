8f1801d93742417fc70b024df590148c3cf6ac13
Chorizo <mateo.ledru@e-artsup.net>
2024-11-06 15:30:01 +0100

    Move et jump

---
+++ Gouzux/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Script sur le joueur lui permettant de sauter si il touche le sol
+/// </summary>
+public class PlayerJump : MonoBehaviour
+{
+    [SerializeField]
+    private float _jumpForce;
+
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    [SerializeField]
+    private float _distance;
+
+    public bool IsGrounded;
+    public static PlayerJump Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void OnJump(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.performed)
+        {
+            DetectHit(transform.localPosition, _distance, new Vector3(0, -1, 0));
+            if (IsGrounded == true)
+            {
+                _rb.AddForce(new Vector2(0, _jumpForce * 10));
+                StartCoroutine(Delay());
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Fonction qui trace un raycast pour savoir si le joueur peut sauter ou non
+    /// </summary>
+    /// <param name="startPos"></param>
+    /// <param name="distance"></param>
+    /// <param name="direction"></param>
+    void DetectHit(Vector3 startPos, float distance, Vector3 direction)
+    {
+        RaycastHit2D hit;
+        Vector3 endPos = startPos + (distance * direction);
+        int layerMask = 1 << 6;
+        hit = Physics2D.Raycast(startPos, direction, distance, layerMask);
+        if (hit.collider == null) //Vérification que l'on touche bien un collider (sinon null ref)
+        {
+            Debug.Log("Attention vous ne touchez rien");
+            return;
+        }
+        if (hit.collider.gameObject.layer == 6)
+        {
+            IsGrounded = true;
+            endPos = hit.point;
+        }
+        Debug.DrawLine(startPos, endPos, Color.red);
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(0.05f);
+        IsGrounded = false;
+    }
+}
---
+++ Gouzux/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Script qui gère les mouvements du joueur
+/// </summary>
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D _rb;
+
+    private Vector2 _inputMovement;
+    private float _speed;
+    private bool _canMove = true;
+
+    public static PlayerMovement Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void OnMove(InputAction.CallbackContext callbackContext)
+    {
+        _inputMovement = callbackContext.ReadValue<Vector2>();
+        if (callbackContext.canceled)
+        {
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
+        }
+    }
+
+    private void Update()
+    {
+        if (_inputMovement.x != 0 && _canMove)
+        {
+            if (_inputMovement.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 0, transform.rotation.z));
+                _rb.velocity = new Vector2(_inputMovement.x * _speed, _rb.velocity.y);
+            }
+            else if (_inputMovement.x < 0)
+            {
+                transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.x, 180, transform.rotation.z));
+                _rb.velocity = new Vector2(_inputMovement.x * _speed, _rb.velocity.y);
+            }
+        }
+    }
+}
