891a318fb5d4656bbecc511f827b21e711553d20
Chorizo <mateo.ledru@e-artsup.net>
2024-11-11 12:51:16 +0100

    jump

--- Gouzux/Assets/Scripts/Player/PlayerMove/PlayerJump.cs
+++ Gouzux/Assets/Scripts/Player/PlayerMove/PlayerJump.cs
@@ -14,9 +14,11 @@ public class PlayerJump : MonoBehaviour
     private Rigidbody2D _rb;
 
     [SerializeField]
-    private float _distance;
+    private LayerMask _groundLayer;
 
-    public bool IsGrounded;
+    [SerializeField]
+    private Transform _groundCheck;
+
     public static PlayerJump Instance;
 
     private void Awake()
@@ -26,46 +28,18 @@ public class PlayerJump : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext callbackContext)
     {
-        if (callbackContext.performed)
+        if (callbackContext.performed && IsGrounded())
         {
-            DetectHit(transform.localPosition, _distance, new Vector3(0, -1, 0));
-            if (IsGrounded == true)
-            {
-                _rb.AddForce(new Vector2(0, _jumpForce * 10));
-                StartCoroutine(Delay());
-            }
+            _rb.velocity = new Vector2(_rb.velocity.x, _jumpForce);
+        }
+        if (callbackContext.canceled && _rb.velocity.y > 0)
+        {
+            _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * 0.5f);
         }
 
     }
-
-    /// <summary>
-    /// Fonction qui trace un raycast pour savoir si le joueur peut sauter ou non
-    /// </summary>
-    /// <param name="startPos"></param>
-    /// <param name="distance"></param>
-    /// <param name="direction"></param>
-    void DetectHit(Vector3 startPos, float distance, Vector3 direction)
+    private bool IsGrounded()
     {
-        RaycastHit2D hit;
-        Vector3 endPos = startPos + (distance * direction);
-        int layerMask = 1 << 6;
-        hit = Physics2D.BoxCast(startPos, new Vector2 (1 ,0.8f), 90, direction, distance, layerMask);
-        if (hit.collider == null) //Vérification que l'on touche bien un collider (sinon null ref)
-        {
-            Debug.Log("Attention vous ne touchez rien");
-            return;
-        }
-        if (hit.collider.gameObject.layer == 6)
-        {
-            IsGrounded = true;
-            endPos = hit.point;
-        }
-        Debug.DrawLine(startPos, endPos, Color.red);
-    }
-
-    IEnumerator Delay()
-    {
-        yield return new WaitForSeconds(0.05f);
-        IsGrounded = false;
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer);
     }
 }
