7443ea70ac60cc464fe93bc95ea9304d58fe99d8
Chorizo6969 <146953802+Chorizo6969@users.noreply.github.com>
2024-11-20 09:02:50 +0100

    zertghjklm

--- Gouzux/Assets/Scripts/Wagon/Movement.cs
+++ Gouzux/Assets/Scripts/Wagon/Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -10,8 +11,9 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private float _speed = 2;
 
-    private void FixedUpdate()
+    private async void FixedUpdate()
     {
+        await Task.Delay(2000);
         // Déplace le wagon horizontalement
         _rb.velocity = new Vector2(_speed, _rb.velocity.y);
     }
