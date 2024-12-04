421c0144c18ab91c75c271c02d193f524d80ac0d
lNoshirol <145337387+lNoshirol@users.noreply.github.com>
2024-12-04 16:00:06 +0100

    truc de fin  et musique tkt

--- Gouzux/Assets/Scripts/Musique.cs
+++ Gouzux/Assets/Scripts/Musique.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class Musique : MonoBehaviour
 {
-    [SerializeField] private AudioClip _audioCLip;
+    [SerializeField] private AudioClip _audioClip;
     [SerializeField] private AudioSource _audioSource;
 
     private void Start()
     {
         _audioSource = GetComponent<AudioSource>();
-        _audioSource.clip = _audioCLip;
+        _audioSource.clip = _audioClip;
         _audioSource.playOnAwake = true;
         _audioSource.loop = true;
         _audioSource.Play();
